44841de6e9c4a32d90d463ec3bba2243f369db3b
shreya-deshmukh09 <deshmukhshreya009@gmail.com>
2026-06-13 18:56:38 +0530

    01 basic project u

---
+++ 01_basicreact/01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+function App() {
+  
+
+  return (
+    <h1>Hello World | Shreya </h1>
+      
+  )
+}
+
+export default App
---
+++ 01_basicreact/01vitereact/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx'
+
+const anotherElement =(
+  <a href='https://google.com' target='_blank'>Visit google</a>
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    anotherElement
+  </StrictMode>,
+)
